0af1ad5e5fc6965dab4bbcbef4df2eeeab7180e9
Onesiphore-chris <chrisleconquerant01@gmail.com>
2023-03-14 14:50:29 +0000

    ajout du dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Col_gestion_employees</title>
+</head>
+   <!-- font aweasome link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/css/lightgallery.min.css">
+
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
+    <!-- cusom css link  -->
+    <link rel="stylesheet" href="css/style.css">
+<body>
+
+
+        <div class="container">
+            <aside>
+                <div class="top">
+                    <div class="logo"><img src="../images/facebook.png" alt=""></div>
+                        <h1 >Col<span class="danger">Entreprise</span></h1>
+                        
+                    <div class="ferme" id="close-btn">
+                        <i class="fa-solid fa-xmark"></i>
+                    </div>
+                </div>
+                    <div class="sidebar">
+                        <a href="">
+                            <i class="fa-solid fa-grid-2"></i>
+                            <h2 class="">Dashboard</h2>
+                        </a>
+                         <a href="" class="active">
+                        <i class="fa-solid fa-gauge-high"></i>
+                            <h2>Dashboard</h2>
+                        </a>
+                        <a href="">
+                            <i class="fa-solid fa-chart-pie"></i>
+                            <h2>Analyse</h2>
+                        </a>
+                        <a href="">
+                        <i class="fa-regular fa-message"></i>
+                            <h2>Messages</h2>
+                            <span class="message-count">26</span>
+                        </a>
+                        <a href="">
+                        <i class="fa-duotone fa-chart-simple"></i>
+                            <h2>Rapports</h2>
+                        </a>
+                        <a href="">
+                        <i class="fa-regular fa-gear"></i>
+                            <h2>Parrametre</h2>
+                        </a>
+                        <a href="">
+                            <i class="fa-solid fa-user-plus"></i>
+                            <h2>Ajouter employee</h2>
+                        </a><a href="">
+                            <i class="fa-solid fa-circle-xmark"></i>
+                            <h2>Exist</h2>
+                        </a>
+                    </div>
+               
+            </aside>
+
+            <!--  END OF ASIDE  AND GO TO MAIN -->
+            <main>
+                <h1>Dashnoard</h1>
+
+                <div class="date">
+                    <input type="date" name="" id="">
+                </div>
+                <div class="insights">
+                    <div class="sales">
+                    <i class="fa-thin fa-chart-simple"></i>
+                    <div class="middle">
+                        <div class="left">
+                            <h3>Temps d'employee</h3>
+                            <h1>stat's</h1>
+                        </div>
+                        <div class="progress">
+                            <svg>
+                                <circle cx='38' cy = '38' r = '35'></circle>
+                            </svg>
+                            <div class="number">
+                                <p>81%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <small class="text-muted">Pendant 24Heures</small>
+                    </div>
+                    <!-- END THE STAST'S ------>
+                    <div class="expenses">
+                    <i class="fa-thin fa-chart-simple"></i>
+                    <div class="middle">
+                        <div class="left">
+                            <h3>Temps d'employee</h3>
+                            <h1>stat's</h1>
+                        </div>
+                        <div class="progress">
+                            <svg>
+                                <circle cx='38' cy = '38' r = '35'></circle>
+                            </svg>
+                            <div class="number">
+                                <p>81%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <small class="text-muted">Pendant 24Heures</small>
+                    </div>
+                    <!-- END THE STAST'S ------>
+                    <div class="income">
+                    <i class="fa-thin fa-chart-simple"></i>
+                    <div class="middle">
+                        <div class="left">
+                            <h3>Temps d'employee</h3>
+                            <h1>stat's</h1>
+                        </div>
+                        <div class="progress">
+                            <svg>
+                                <circle cx='38' cy = '38' r = '35'></circle>
+                            </svg>
+                            <div class="number">
+                                <p>81%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <small class="text-muted">Pendant 24Heures</small>
+                    </div>
+                    <!-- END THE STAST'S ------>
+
+                    <!-- END THE MAAIN-INS ------>
+
+                    <div class="recent-order">
+                        <h2>Activité Recente</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Person Name</th>
+                                    <th>Person Number</th>
+                                    <th>Payement Status</th>
+                                    <th>status</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Chris le conquerant</td>
+                                    <td>225 07 69 36 28 51</td>
+                                    <td>Payement Validé</td>
+                                    <td class="warring">Administrateur</td>
+                                    <td class="primary">Details</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="">Click</a>
+                    </div>
+
+                    
+                </div>
+            </main>
+            
+        </div>
+</body>
+</html>
